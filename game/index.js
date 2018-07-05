e45922723cbafa189b3e52241f07cd3b68aba120
Datguyovrder <rhui@hawk.iit.edu>
2018-07-05 11:57:53 -0500

    xoxo

--- game/index.js
+++ game/index.js
@@ -1,6 +1,31 @@
 import {Map} from 'immutable'
+import {createStore} from "redux"
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+const empty = Map()
+
+const defaultState = {
+  turn: "O",
+  board: empty
 }
+
+//Action types
+const MOVE = "MOVE"
+
+//Action creators
+export const move = (turn, coord) => ({type: MOVE, turn, coord})
+
+
+//Reducer
+export default function reducer(state = defaultState, action) {
+  // TODO
+  switch(action.type){
+    case MOVE:
+      const newBoard = state.board.setIn(action.coord, state.turn)
+      return ({turn: state.turn === "O" ? "X": "O", board: newBoard})
+    default:
+      return state
+  }
+}
+
+//Store
+// const store = createStore(reducer)
